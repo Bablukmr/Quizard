b72a05315b84be63ac9798105da12c4b4ab8db45
Bablukmr <bablukmr800@gmail.com>
2024-03-09 07:42:31 +0530

    Dashboard

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
-import Header from './header'
+import React, { useState } from "react";
+import Header from "./header";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Dashboard() {
-  return (
-    <div>
-      <Header/>
+  const [boxes, setBoxes] = useState([]);
 
+  const handleAddBox = () => {
+    setBoxes([...boxes, { id: boxes.length }]);
+  };
+
+  const handleDeleteBox = (id) => {
+    setBoxes(boxes.filter((box) => box.id !== id));
+  };
+
+  return (
+    <div className="m-0 p-0">
+      <div>
+        <Header />
+      </div>
+
+      <div className="w-[100vw] mt-[5px] h-[calc(100vh-70px)] flex items-center justify-center">
+        <div className="w-[15%] h-full flex items-start flex-col overflow-y-auto border-r-2 gap-4 px-5 pt-4">
+          {boxes.map((box, index) => (
+            <div
+              key={box.id}
+              className="border-2 border-gray-400 text-gray-400 text-2xl  w-full min-h-[120px] hover:cursor-pointer flex items-center justify-center rounded-lg pt-2 relative"
+            >
+              <span className="absolute top-0 left-0 ml-2 mt-2 text-xs text-gray-500">
+                Question {index + 1}
+              </span>
+              <button
+                className="absolute top-0 right-0 mr-2 mt-2 text-gray-500 hover:text-red-500"
+                onClick={() => handleDeleteBox(box.id)}
+              >
+                <DeleteIcon />
+              </button>
+            </div>
+          ))}
+
+          <div
+            className="border-2 border-gray-400 text-gray-400 text-2xl  w-full min-h-[120px] hover:cursor-pointer flex items-center justify-center rounded-lg py-2"
+            onClick={handleAddBox}
+          >
+            <AddIcon />
+          </div>
+        </div>
+        <div className="w-[85%] h-full flex items-center justify-center"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/header.jsx
+++ src/header.jsx
@@ -1,20 +1,29 @@
-import { Button } from '@mui/material'
-import SaveIcon from '@mui/icons-material/Save';
-import React from 'react'
+import SaveIcon from "@mui/icons-material/Save";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import React from "react";
 
 function Header() {
   return (
     <div>
-        <div className='flex justify-between px-5 py-2'>
-            <div>
-                <h1 className='text-gray-600 text-2xl font-bold'>Quizard</h1>
-            </div>
-            <div>
-            <Button variant="contained"><SaveIcon/> Contained</Button>
-            </div>
+      <div className="flex h-[70px] justify-between px-5 py-3 border-b-2">
+        <div>
+          <h1 className="text-gray-600 text-2xl font-bold">Quizard</h1>
         </div>
+        <div className="flex items-center justify-center gap-x-3">
+          <button className="px-5 py-[6px] bg-slate-200 rounded-md">
+            <ArrowLeftIcon />
+            Preview
+          </button>
+          <button className="px-5 py-[6px] flex items-center justify-center gap-1 bg-blue-800 text-white rounded-md hover:shadow-lg">
+            <p className=" text-sm">
+              <SaveIcon style={{ fontSize: 18 }} />
+            </p>
+            Publish
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
